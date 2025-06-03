7ecd9734a11cadefe713f4b0dcc1337fd8f46ba0
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-06-03 21:23:49 -0230

    dont need user schema with sequelize

--- src/schemas/userSchema.ts
+++
@@ -1,9 +0,0 @@
-const userSchema = `
-  CREATE TABLE IF NOT EXISTS users (
-    userId   TEXT    PRIMARY KEY,
-    email    TEXT    NOT NULL UNIQUE,
-    password TEXT    NOT NULL
-  );
-`;
-
-export default userSchema;
