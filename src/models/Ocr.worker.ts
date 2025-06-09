638fc6ff21b22f4f2b29781124115be1b3125a82
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-06-09 06:17:33 -0230

    ocr works, gets files from file upload

--- src/models/Ocr.worker.ts
+++ src/models/Ocr.worker.ts
@@ -1,11 +1,48 @@
 import { createWorker } from "tesseract.js";
+import path from "path";
+import fs from "fs/promises";
+import FileUpload from "./FileUpload.js";
 
 export async function runOCR() {
-  const worker =  await createWorker('eng');
+  const latestUpload = await FileUpload.findOne({
+    order: [["createdAt", "DESC"]],
+  });
+
+  if (!latestUpload) throw new Error("no file found in FileUpload table.");
+
+  const absoluteImagePath = path.join(
+    process.cwd(),
+    "uploads",
+    latestUpload.filename
+  );
+
+  const worker = await createWorker();
+  await worker.load("eng");
+  await worker.reinitialize("eng");
+
   const {
     data: { text },
-  } = await worker.recognize("./uploads/1749447054062-95681880.jpeg");
-  console.log(text);
+  } = await worker.recognize(absoluteImagePath);
+
   await worker.terminate();
-  return text;
+
+  const result = {
+    userId: latestUpload.userId,
+    filename: latestUpload.filename,
+    ocrText: text,
+  };
+
+  const utilsDir = path.join(process.cwd(), "src/utils");
+  const outputPath = path.join(utilsDir, "ocr.result.json");
+
+  try {
+    await fs.mkdir(utilsDir, { recursive: true });
+  } catch (err) {
+    console.error("error in making utils directory:", err);
+  }
+
+  await fs.writeFile(outputPath, JSON.stringify(result, null, 2));
+
+  console.log("OCR completed and saved");
+  return result;
 }
