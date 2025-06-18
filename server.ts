4cfa2ccf6cdcc3318a6256db8b605bbca62357cf
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-06-18 16:01:03 -0230

    server.ts and match update route

--- server.ts
+++ server.ts
@@ -8,6 +8,8 @@ import uploadRoutes from "./src/routes/upload.route.js";
 import userRoutes from "./src/routes/user.route.js";
 import "./src/controllers/unlink.controller.js";
 import ocrRoutes from "./src/routes/ocr.route.js";
+import parsedRoutes from "./src/routes/parsed.route.js";
+import saveDataRouter from "./src/routes/savedata.route.js";
 
 const app = express();
 const port = process.env.PORT || 5050;
@@ -18,10 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", authRoutes);
 app.use("/", uploadRoutes);
 app.use("/ocr", ocrRoutes);
+app.use("/parsed", parsedRoutes);
+app.use("/savedata", saveDataRouter);
 app.use("/uploads", express.static("uploads"));
 app.use("/", userRoutes);
 
 connectDB();
+
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
